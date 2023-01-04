a3b2ecdce69f450e4298e10b1bd7518c010b501e
Maurits Silvis <mauritssilvis@gmail.com>
2023-01-04 16:19:32 +0100

    Add tests for cases without paths

--- checkouts/java/src/test/java/nl/mauritssilvis/darts/checkouts/java/paths/PathFinderTests.java
+++ checkouts/java/src/test/java/nl/mauritssilvis/darts/checkouts/java/paths/PathFinderTests.java
@@ -9,22 +9,48 @@ import org.junit.jupiter.api.Assertions;
 import org.junitpioneer.jupiter.cartesian.ArgumentSets;
 import org.junitpioneer.jupiter.cartesian.CartesianTest;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
 class PathFinderTests {
     @CartesianTest
-    @CartesianTest.MethodFactory("emptyStepFactory")
-    void handleEmptySteps(PathFinder pathFinder, List<Set<Integer>> steps, int target) {
+    @CartesianTest.MethodFactory("withAllPathFinders")
+    void handleAbsentSteps(PathFinder pathFinder) {
+        List<Set<Integer>> steps = new ArrayList<>();
+        int target = 0;
+
         Set<List<Integer>> paths = pathFinder.find(steps, target);
+
         Assertions.assertEquals(0, paths.size());
     }
 
-    static ArgumentSets emptyStepFactory() {
+    @CartesianTest
+    @CartesianTest.MethodFactory("withEmptySteps")
+    void handleEmptySteps(PathFinder pathFinder, List<Set<Integer>> steps, int target) {
+        Set<List<Integer>> paths = pathFinder.find(steps, target);
+
+        Assertions.assertEquals(0, paths.size());
+    }
+
+    @CartesianTest
+    @CartesianTest.MethodFactory("withUnreachableTargets")
+    void handleUnreachableTargets(PathFinder pathFinder, List<Set<Integer>> steps, int target) {
+        Set<List<Integer>> paths = pathFinder.find(steps, target);
+
+        Assertions.assertEquals(0, paths.size());
+    }
+
+    private static ArgumentSets withAllPathFinders() {
+        return ArgumentSets
+                .argumentsForFirstParameter(getPathFinders());
+    }
+
+    static ArgumentSets withEmptySteps() {
         return ArgumentSets
                 .argumentsForFirstParameter(
-                        new NaivePathFinder()
+                        getPathFinders()
                 )
                 .argumentsForNextParameter(
                         List.of(Collections.emptySet()),
@@ -36,5 +62,29 @@ class PathFinderTests {
                 .argumentsForNextParameter(
                         2
                 );
+    }
+
+    static ArgumentSets withUnreachableTargets() {
+        return ArgumentSets
+                .argumentsForFirstParameter(
+                        getPathFinders()
+                )
+                .argumentsForNextParameter(
+                        List.of(Set.of(1)),
+                        List.of(Set.of(-3, 1)),
+                        List.of(Set.of(1), Set.of(2)),
+                        List.of(Set.of(0, 2), Set.of(3, -4)),
+                        List.of(Set.of(1), Set.of(1), Set.of(1)),
+                        List.of(Set.of(0), Set.of(1, 2), Set.of(2, 3))
+                )
+                .argumentsForNextParameter(
+                        -1, 0, 2
+                );
+    }
+
+    static List<PathFinder> getPathFinders() {
+        return List.of(
+                new NaivePathFinder()
+        );
     }
 }
