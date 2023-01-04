3e3ff249e4c95260b9a799bfb1553acf8b12b375
Maurits Silvis <mauritssilvis@gmail.com>
2023-01-04 15:36:17 +0100

    Set up Cartesian path finder tests

--- checkouts/java/src/test/java/nl/mauritssilvis/darts/checkouts/java/paths/PathFinderTests.java
+++ checkouts/java/src/test/java/nl/mauritssilvis/darts/checkouts/java/paths/PathFinderTests.java
@@ -6,22 +6,35 @@
 package nl.mauritssilvis.darts.checkouts.java.paths;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junitpioneer.jupiter.cartesian.ArgumentSets;
+import org.junitpioneer.jupiter.cartesian.CartesianTest;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
 class PathFinderTests {
-    @Test
-    void findSimplePath() {
-        List<Set<Integer>> steps = List.of(
-                Set.of(1), Set.of(1)
-        );
-        int target = 2;
-
-        PathFinder pathFinder = new NaivePathFinder();
+    @CartesianTest
+    @CartesianTest.MethodFactory("emptyStepFactory")
+    void handleEmptySteps(PathFinder pathFinder, List<Set<Integer>> steps, int target) {
         Set<List<Integer>> paths = pathFinder.find(steps, target);
+        Assertions.assertEquals(0, paths.size());
+    }
 
-        Assertions.assertEquals(1, paths.size());
+    static ArgumentSets emptyStepFactory() {
+        return ArgumentSets
+                .argumentsForFirstParameter(
+                        new NaivePathFinder()
+                )
+                .argumentsForNextParameter(
+                        List.of(Collections.emptySet()),
+                        List.of(Collections.emptySet(), Collections.emptySet()),
+                        List.of(Collections.emptySet(), Set.of(1, 2)),
+                        List.of(Set.of(1, 2), Collections.emptySet()),
+                        List.of(Set.of(3), Collections.emptySet(), Set.of(-1))
+                )
+                .argumentsForNextParameter(
+                        2
+                );
     }
 }
