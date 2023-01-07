3287ff582f55c8a0fb829a248ad661143e5dd100
Maurits Silvis <mauritssilvis@gmail.com>
2023-01-07 18:50:08 +0100

    Add grouped-path multiplicity tests

--- checkouts/java/src/test/java/nl/mauritssilvis/darts/checkouts/java/paths/GroupedPathTests.java
+++ checkouts/java/src/test/java/nl/mauritssilvis/darts/checkouts/java/paths/GroupedPathTests.java
@@ -7,11 +7,15 @@ package nl.mauritssilvis.darts.checkouts.java.paths;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
 
 class GroupedPathTests {
     @Test
@@ -187,5 +191,87 @@ class GroupedPathTests {
         Path path = GroupedPath.of(steps, grouping);
 
         Assertions.assertEquals(1, path.getGroupCount());
+    }
+
+    @ParameterizedTest
+    @MethodSource("withMultiplicityData")
+    void getThePathMultiplicity(
+            Collection<Integer> steps,
+            Collection<Boolean> grouping,
+            int multiplicity) {
+
+        Path path = GroupedPath.of(steps, grouping);
+        Assertions.assertEquals(multiplicity, path.getMultiplicity());
+    }
+
+    static Stream<Arguments> withMultiplicityData() {
+        return Stream.of(
+                Arguments.arguments(
+                        Collections.emptyList(), Collections.emptyList(), 0
+                ),
+                Arguments.arguments(
+                        List.of(3), List.of(false), 1
+                ),
+                Arguments.arguments(
+                        List.of(2, 4), List.of(false, true), 2
+                ),
+                Arguments.arguments(
+                        List.of(5, 5, 5), List.of(true, true, true), 1
+                ),
+                Arguments.arguments(
+                        List.of(3, 7, 7, 3),
+                        List.of(true, true, true, true),
+                        6
+                ),
+                Arguments.arguments(
+                        List.of(9, 8, 2, 2, 2),
+                        List.of(false, true, true, true, true),
+                        20
+                ),
+                Arguments.arguments(
+                        List.of(2, 2, 1, 2, 2),
+                        List.of(true, true, false, true, true),
+                        3
+                ),
+                Arguments.arguments(
+                        List.of(1, 8, 4, 0, 6, 3),
+                        List.of(false, true, true, true, true, true),
+                        720
+                ),
+                Arguments.arguments(
+                        List.of(1, 2, 6, 7, 7, 2, 7),
+                        List.of(false, true, true, false, true, true, true),
+                        6 * 4
+                ),
+                Arguments.arguments(
+                        List.of(1, 3, 5, 7, 9, 6, 6),
+                        List.of(false, true, false, true, true, true, true),
+                        2 * 60
+                ),
+                Arguments.arguments(
+                        List.of(7, 8, 7, 8, 7, 8, 7),
+                        List.of(false, true, true, true, true, true, false),
+                        60
+                ),
+                Arguments.arguments(
+                        List.of(3, 4, 4, 5, 6, 6, 6, 6),
+                        List.of(false, true, true, true, false, true, true, true),
+                        12
+                ),
+                Arguments.arguments(
+                        List.of(1, 6, 6, 5, 5, 2, 3, 3, 1),
+                        List.of(true, false, true, true, true, true, false, true, true),
+                        30 * 3
+                ),
+                Arguments.arguments(
+                        List.of(1, 2, 2, 3, 7, 8, 7, 7, 2, 3),
+                        List.of(true, true, true, true, false, false, true, true, false, true),
+                        12 * 3 * 2),
+                Arguments.arguments(
+                        List.of(1, 1, 1, 3, 3, 2, 2, 3, 3, 4, 4),
+                        List.of(false, true, true, true, true, false, true, true, true, true, true),
+                        10 * 90
+                )
+        );
     }
 }
