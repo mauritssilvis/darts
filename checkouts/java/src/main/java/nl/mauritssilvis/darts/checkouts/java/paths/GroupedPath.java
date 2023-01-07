847293f822e1dd78ef0130f39c53e25d9a038dee
Maurits Silvis <mauritssilvis@gmail.com>
2023-01-07 13:06:25 +0100

    Start setting up a path implementation representing multiple sequences

---
+++ checkouts/java/src/main/java/nl/mauritssilvis/darts/checkouts/java/paths/GroupedPath.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright © 2023 Maurits H. Silvis
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+package nl.mauritssilvis.darts.checkouts.java.paths;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * An implementation of the {@code Path} interface that can represent multiple
+ * sequences of integer steps, namely, one for each permutation of the grouped
+ * elements.
+ */
+public class GroupedPath implements Path {
+    private final List<Integer> steps;
+    private final List<Boolean> grouping;
+
+    private GroupedPath(Stream<Integer> steps, Stream<Boolean> grouping) {
+        this.steps = steps.toList();
+        this.grouping = grouping.limit(this.steps.size())
+                .toList();
+    }
+
+    public static GroupedPath of(Collection<Integer> steps, Collection<Boolean> grouping) {
+        return new GroupedPath(steps.stream(), grouping.stream());
+    }
+
+    public static GroupedPath of(int[] steps, boolean[] grouping) {
+        return new GroupedPath(
+                Arrays.stream(steps).boxed(),
+                IntStream.range(0, grouping.length)
+                        .mapToObj(i -> grouping[i])
+        );
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public int getLength() {
+        return 0;
+    }
+
+    @Override
+    public List<Integer> getSteps() {
+        return null;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return 0;
+    }
+
+    @Override
+    public int getMultiplicity() {
+        return 0;
+    }
+}
