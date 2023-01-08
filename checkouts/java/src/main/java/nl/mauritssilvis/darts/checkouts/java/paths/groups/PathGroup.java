67113baaa11035fd167ca597a556b5b0f704dcba
Maurits Silvis <mauritssilvis@gmail.com>
2023-01-08 10:59:49 +0100

    Implement path groups

--- checkouts/java/src/main/java/nl/mauritssilvis/darts/checkouts/java/paths/groups/PathGroup.java
+++ checkouts/java/src/main/java/nl/mauritssilvis/darts/checkouts/java/paths/groups/PathGroup.java
@@ -6,20 +6,65 @@
 package nl.mauritssilvis.darts.checkouts.java.paths.groups;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class PathGroup implements Group {
+    private final List<Integer> values;
+
+    private PathGroup(Collection<Integer> values) {
+        this.values = List.copyOf(values);
+    }
+
     public static Group of(Collection<Integer> values) {
-        return new PathGroup();
+        return new PathGroup(values);
     }
 
     @Override
     public List<Integer> getValues() {
-        return null;
+        return values;
     }
 
     @Override
     public long countPermutations() {
-        return -1;
+        if (values.isEmpty()) {
+            return 0;
+        } else if (values.size() == 1) {
+            return 1;
+        }
+
+        Map<Integer, Long> frequencies = getFrequencies();
+
+        long denominator = frequencies.values().stream()
+                .map(this::factorial)
+                .reduce(1L, (p, e) -> p * e);
+
+        long numerator = factorial(values.size());
+
+        return numerator / denominator;
+    }
+
+    private Map<Integer, Long> getFrequencies() {
+        return values.stream()
+                .collect(
+                        Collectors.groupingBy(
+                                Function.identity(),
+                                HashMap::new,
+                                Collectors.counting()
+                        )
+                );
+    }
+
+    private long factorial(long in) {
+        long out = 1;
+
+        for (int i = 2; i <= in; i++) {
+            out *= i;
+        }
+
+        return out;
     }
 }
