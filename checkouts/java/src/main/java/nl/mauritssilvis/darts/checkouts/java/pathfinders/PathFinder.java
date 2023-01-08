77b8e07b166216c919199f3a8cd5f4f769565acf
Maurits Silvis <mauritssilvis@gmail.com>
2023-01-08 16:05:56 +0100

    Start documenting the functional path finder interface

--- checkouts/java/src/main/java/nl/mauritssilvis/darts/checkouts/java/pathfinders/PathFinder.java
+++ checkouts/java/src/main/java/nl/mauritssilvis/darts/checkouts/java/pathfinders/PathFinder.java
@@ -9,6 +9,19 @@ import nl.mauritssilvis.darts.checkouts.java.paths.Path;
 
 import java.util.List;
 
+/**
+ * A tool to find paths of a certain length.
+ * <p>
+ * This interface does not fix the data structure that is searched for paths.
+ * Implementations can specify the data structures they apply to.
+ */
+@FunctionalInterface
 public interface PathFinder {
+    /**
+     * Finds paths having a specified length.
+     *
+     * @param length the target path length
+     * @return a list of paths having the given length
+     */
     List<Path> find(int length);
 }
