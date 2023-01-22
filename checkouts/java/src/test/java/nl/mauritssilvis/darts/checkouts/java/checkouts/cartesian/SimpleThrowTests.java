1dd50b18ca9e2589b66c2a2aa618fbe113064b0b
Maurits Silvis <mauritssilvis@gmail.com>
2023-01-22 23:05:30 +0100

    Start testing equality of simple throws

--- checkouts/java/src/test/java/nl/mauritssilvis/darts/checkouts/java/checkouts/cartesian/SimpleThrowTests.java
+++ checkouts/java/src/test/java/nl/mauritssilvis/darts/checkouts/java/checkouts/cartesian/SimpleThrowTests.java
@@ -50,4 +50,29 @@ class SimpleThrowTests {
                 () -> storedFields.add(field)
         );
     }
+
+    @Test
+    void getEqualThrows() {
+        Field field1 = TypedFieldTestUtils.getField("19");
+        Throw simpleThrow1 = SimpleThrow.of(field1);
+
+        Field field2 = TypedFieldTestUtils.getField("19");
+        Throw simpleThrow2 = SimpleThrow.of(field2);
+
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(simpleThrow1, simpleThrow2),
+                () -> Assertions.assertEquals(simpleThrow1.hashCode(), simpleThrow2.hashCode())
+        );
+    }
+
+    @Test
+    void getUnequalThrows() {
+        Field field1 = TypedFieldTestUtils.getField("25");
+        Throw simpleThrow1 = SimpleThrow.of(field1);
+
+        Field field2 = TypedFieldTestUtils.getField("D25");
+        Throw simpleThrow2 = SimpleThrow.of(field2);
+
+        Assertions.assertNotEquals(simpleThrow1, simpleThrow2);
+    }
 }
