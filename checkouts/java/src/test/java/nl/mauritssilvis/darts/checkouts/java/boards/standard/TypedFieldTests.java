f9bbcd9e6793a9c6cbf5edcad2cbe6dfb382feba
Maurits Silvis <mauritssilvis@gmail.com>
2023-01-22 22:21:48 +0100

    Start testing equality of typed fields

--- checkouts/java/src/test/java/nl/mauritssilvis/darts/checkouts/java/boards/standard/TypedFieldTests.java
+++ checkouts/java/src/test/java/nl/mauritssilvis/darts/checkouts/java/boards/standard/TypedFieldTests.java
@@ -100,7 +100,7 @@ class TypedFieldTests {
     }
 
     @Test
-    void getTheTypeOfASingleField() {
+    void getTheFieldTypeOfASingleField() {
         FieldType fieldType = FieldType.SINGLE;
         int baseScore = 19;
         Field field = TypedField.of(fieldType, baseScore);
@@ -109,7 +109,7 @@ class TypedFieldTests {
     }
 
     @Test
-    void getTheTypeOfADoubleField() {
+    void getTheFieldTypeOfADoubleField() {
         FieldType fieldType = FieldType.DOUBLE;
         int baseScore = 5;
         Field field = TypedField.of(fieldType, baseScore);
@@ -118,7 +118,7 @@ class TypedFieldTests {
     }
 
     @Test
-    void getTheTypeOfATripleField() {
+    void getTheFieldTypeOfATripleField() {
         FieldType fieldType = FieldType.TRIPLE;
         int baseScore = 17;
         Field field = TypedField.of(fieldType, baseScore);
@@ -127,11 +127,65 @@ class TypedFieldTests {
     }
 
     @Test
-    void getTheTypeOfAQuadrupleField() {
+    void getTheFieldTypeOfAQuadrupleField() {
         FieldType fieldType = FieldType.QUADRUPLE;
         int baseScore = 4;
         Field field = TypedField.of(fieldType, baseScore);
 
         Assertions.assertEquals(fieldType, field.getFieldType());
+    }
+
+    @Test
+    void getEqualFields() {
+        FieldType fieldType = FieldType.DOUBLE;
+        int baseScore = 6;
+
+        Field field1 = TypedField.of(fieldType, baseScore);
+        Field field2 = TypedField.of(fieldType, baseScore);
+
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(field1, field2),
+                () -> Assertions.assertEquals(field1.hashCode(), field2.hashCode())
+        );
+    }
+
+    @Test
+    void getUnequalFields() {
+
+        FieldType fieldType1 = FieldType.SINGLE;
+        int baseScore1 = 9;
+        Field field1 = TypedField.of(fieldType1, baseScore1);
+
+        FieldType fieldType2 = FieldType.DOUBLE;
+        int baseScore2 = 10;
+        Field field2 = TypedField.of(fieldType2, baseScore2);
+
+        Assertions.assertNotEquals(field1, field2);
+    }
+
+    @Test
+    void getUnequalFieldsForDifferentFieldTypes() {
+        int baseScore = 9;
+
+        FieldType fieldType1 = FieldType.TRIPLE;
+        Field field1 = TypedField.of(fieldType1, baseScore);
+
+        FieldType fieldType2 = FieldType.QUADRUPLE;
+        Field field2 = TypedField.of(fieldType2, baseScore);
+
+        Assertions.assertNotEquals(field1, field2);
+    }
+
+    @Test
+    void getUnequalFieldsForDifferentBaseScores() {
+        FieldType fieldType = FieldType.TRIPLE;
+
+        int baseScore1 = 9;
+        Field field1 = TypedField.of(fieldType, baseScore1);
+
+        int baseScore2 = 10;
+        Field field2 = TypedField.of(fieldType, baseScore2);
+
+        Assertions.assertNotEquals(field1, field2);
     }
 }
