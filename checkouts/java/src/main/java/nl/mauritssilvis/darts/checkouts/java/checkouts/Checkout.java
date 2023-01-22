d9e12aa08297d977f24191b15b3c984fc21ba6d7
Maurits Silvis <mauritssilvis@gmail.com>
2023-01-22 21:25:27 +0100

    Change the requirements on checkout implementations

--- checkouts/java/src/main/java/nl/mauritssilvis/darts/checkouts/java/checkouts/Checkout.java
+++ checkouts/java/src/main/java/nl/mauritssilvis/darts/checkouts/java/checkouts/Checkout.java
@@ -11,11 +11,12 @@ import java.util.List;
  * A checkout that represents zero or more sequences of throws of a certain
  * total score.
  * <p>
- * Simple checkouts represent a single sequence of throws. In such checkouts,
- * the different throws are not grouped. Interchanging the throws of a simple
- * checkout leads to a sequence of throws that is not represented by that same
- * checkout. In contrast, checkouts with grouped throws represent multiple
- * sequences, one for each permutation of the grouped elements.
+ * Simple checkouts represent a single sequence of simple throws. In such
+ * checkouts, the different throws contain a single field and are not grouped.
+ * Interchanging the throws of a simple checkout leads to a sequence of throws
+ * that is not represented by that same checkout. In contrast, checkouts with
+ * grouped throws represent multiple sequences, one for each permutation of the
+ * grouped elements.
  * <p>
  * Relevant design patterns: Immutable interface.
  */
@@ -45,10 +46,10 @@ public interface Checkout {
     /**
      * Gets the multiplicity of this checkout.
      * <p>
-     * Simple checkouts, which represent a single sequence of throws, have a
-     * multiplicity given by the product of the number of fields of each throw.
-     * Checkouts with grouped throws represent multiple sequences and,
-     * correspondingly, have a larger multiplicity.
+     * Simple checkouts, which represent a single sequence of simple throws,
+     * have a unit multiplicity. Checkouts with grouped throws represent
+     * multiple sequences and, correspondingly, have a multiplicity larger than
+     * one.
      *
      * @return the multiplicity of this checkout
      */
