03ad73ef6a3887ed75c3b4753583b33842b40b9e
Maurits Silvis <mauritssilvis@gmail.com>
2023-01-22 23:31:55 +0100

    Seal the typed field utility class

--- checkouts/java/src/test/java/nl/mauritssilvis/darts/checkouts/java/checkouts/common/TypedFieldTestUtils.java
+++ checkouts/java/src/test/java/nl/mauritssilvis/darts/checkouts/java/checkouts/common/TypedFieldTestUtils.java
@@ -13,7 +13,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class TypedFieldTestUtils {
+public final class TypedFieldTestUtils {
     private TypedFieldTestUtils() {
     }
 
