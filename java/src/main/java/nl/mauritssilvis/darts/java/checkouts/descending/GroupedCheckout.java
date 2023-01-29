fa096c63c32dcd9582a07bcf87dbe76e5584e387
Maurits Silvis <mauritssilvis@gmail.com>
2023-01-29 22:59:30 +0100

    Partly implement grouped checkouts

--- java/src/main/java/nl/mauritssilvis/darts/java/checkouts/descending/GroupedCheckout.java
+++ java/src/main/java/nl/mauritssilvis/darts/java/checkouts/descending/GroupedCheckout.java
@@ -8,8 +8,11 @@ package nl.mauritssilvis.darts.java.checkouts.descending;
 import nl.mauritssilvis.darts.java.checkouts.Checkout;
 import nl.mauritssilvis.darts.java.checkouts.Throw;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
 /**
  * An implementation of the {@code Checkout} interface that represents multiple
@@ -23,9 +26,12 @@ public final class GroupedCheckout implements Checkout {
     private final int score;
 
     private GroupedCheckout(Collection<Throw> throwList, Collection<Boolean> grouping) {
-        this.throwList = null;
-        this.grouping = null;
-        score = -1;
+        this.throwList = List.copyOf(throwList);
+        this.grouping = processGrouping(grouping, throwList.size());
+
+        score = throwList.stream()
+                .mapToInt(Throw::getScore)
+                .sum();
     }
 
     /**
@@ -44,12 +50,12 @@ public final class GroupedCheckout implements Checkout {
 
     @Override
     public int getScore() {
-        return -1;
+        return score;
     }
 
     @Override
     public int countThrows() {
-        return -1;
+        return throwList.size();
     }
 
     @Override
@@ -60,5 +66,26 @@ public final class GroupedCheckout implements Checkout {
     @Override
     public long getMultiplicity() {
         return -1;
+    }
+
+    private static List<Boolean> processGrouping(Collection<Boolean> input, int size) {
+        if (size == 0) {
+            return Collections.emptyList();
+        } else if (size == 1) {
+            return List.of(false);
+        }
+
+        List<Boolean> output = new ArrayList<>();
+        output.add(false);
+
+        input.stream()
+                .limit(size)
+                .skip(1)
+                .forEach(output::add);
+
+        IntStream.range(input.size(), size)
+                .forEach(i -> output.add(false));
+
+        return Collections.unmodifiableList(output);
     }
 }
