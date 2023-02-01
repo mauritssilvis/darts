8d0d1be50329db960e21f555b091ad86ab5b154e
Maurits Silvis <mauritssilvis@gmail.com>
2023-02-01 17:54:09 +0100

    Let the mapped checkout table generator accept a pathfinder factory

--- java/src/main/java/nl/mauritssilvis/darts/java/tables/map/MappedCheckoutTableGenerator.java
+++ java/src/main/java/nl/mauritssilvis/darts/java/tables/map/MappedCheckoutTableGenerator.java
@@ -5,10 +5,15 @@
 
 package nl.mauritssilvis.darts.java.tables.map;
 
+import nl.mauritssilvis.darts.java.paths.Pathfinder;
+import nl.mauritssilvis.darts.java.paths.common.Node;
 import nl.mauritssilvis.darts.java.settings.BoardType;
 import nl.mauritssilvis.darts.java.settings.CheckType;
 import nl.mauritssilvis.darts.java.tables.CheckoutTable;
 import nl.mauritssilvis.darts.java.tables.CheckoutTableGenerator;
+
+import java.util.Collection;
+import java.util.function.Function;
 
 /**
  * An implementation of the {@code CheckoutTableGenerator} interface that
@@ -17,21 +22,32 @@ import nl.mauritssilvis.darts.java.tables.CheckoutTableGenerator;
  * Relevant design patterns: Immutable object, static factory method.
  */
 public class MappedCheckoutTableGenerator implements CheckoutTableGenerator {
-    private MappedCheckoutTableGenerator(BoardType boardType, CheckType checkInType, CheckType checkoutType) {
+    private MappedCheckoutTableGenerator(
+            BoardType boardType,
+            CheckType checkInType,
+            CheckType checkoutType,
+            Function<Collection<? extends Node>, Pathfinder> pathfinderFactory
+    ) {
     }
 
     /**
      * Returns a new {@code MappedCheckoutTableGenerator} with the specified
      * dartboard, check-in and checkout types.
      *
-     * @param boardType    the dartboard type
-     * @param checkInType  the check-in type
-     * @param checkoutType the checkout type
+     * @param boardType         the dartboard type
+     * @param checkInType       the check-in type
+     * @param checkoutType      the checkout type
+     * @param pathfinderFactory a pathfinder factory
      * @return a new {@code MappedCheckoutTableGenerator} with the specified
      * dartboard, check-in and checkout types.
      */
-    public static CheckoutTableGenerator of(BoardType boardType, CheckType checkInType, CheckType checkoutType) {
-        return new MappedCheckoutTableGenerator(boardType, checkInType, checkoutType);
+    public static CheckoutTableGenerator of(
+            BoardType boardType,
+            CheckType checkInType,
+            CheckType checkoutType,
+            Function<Collection<? extends Node>, Pathfinder> pathfinderFactory
+    ) {
+        return new MappedCheckoutTableGenerator(boardType, checkInType, checkoutType, pathfinderFactory);
     }
 
     @Override
