e537be2456f7697a8b592bc46beb0b43626d5da2
Maurits Silvis <mauritssilvis@gmail.com>
2023-02-01 17:34:22 +0100

    Set up a mapped checkout table generator class

---
+++ java/src/main/java/nl/mauritssilvis/darts/java/tables/map/MappedCheckoutTableGenerator.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright © 2023 Maurits H. Silvis
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+package nl.mauritssilvis.darts.java.tables.map;
+
+import nl.mauritssilvis.darts.java.settings.BoardType;
+import nl.mauritssilvis.darts.java.settings.CheckType;
+import nl.mauritssilvis.darts.java.tables.CheckoutTable;
+import nl.mauritssilvis.darts.java.tables.CheckoutTableGenerator;
+
+/**
+ * An implementation of the {@code CheckoutTableGenerator} interface that
+ * generates {@code MappedCheckoutTable} objects.
+ * <p>
+ * Relevant design patterns: Immutable object, static factory method.
+ */
+public class MappedCheckoutTableGenerator implements CheckoutTableGenerator {
+    private MappedCheckoutTableGenerator(BoardType boardType, CheckType checkInType, CheckType checkoutType) {
+    }
+
+    /**
+     * Returns a new {@code MappedCheckoutTableGenerator} with the specified
+     * dartboard, check-in and checkout types.
+     *
+     * @param boardType    the dartboard type
+     * @param checkInType  the check-in type
+     * @param checkoutType the checkout type
+     * @return a new {@code MappedCheckoutTableGenerator} with the specified
+     * dartboard, check-in and checkout types.
+     */
+    public static CheckoutTableGenerator of(BoardType boardType, CheckType checkInType, CheckType checkoutType) {
+        return new MappedCheckoutTableGenerator(boardType, checkInType, checkoutType);
+    }
+
+    @Override
+    public CheckoutTable generate(int minScore, int maxScore) {
+        return null;
+    }
+}
