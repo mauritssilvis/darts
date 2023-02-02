c4bf1ac1345826766e5405e73209f7ce94b14411
Maurits Silvis <mauritssilvis@gmail.com>
2023-02-02 14:24:44 +0100

    Start setting up a Quadro dartboard class

---
+++ java/src/main/java/nl/mauritssilvis/darts/java/boards/quadro/QuadroBoard.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright © 2023 Maurits H. Silvis
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+package nl.mauritssilvis.darts.java.boards.quadro;
+
+import nl.mauritssilvis.darts.java.boards.Board;
+import nl.mauritssilvis.darts.java.boards.Field;
+import nl.mauritssilvis.darts.java.boards.FieldType;
+
+import java.util.List;
+
+/**
+ * An implementation of the {@code Board} interface that represents a Quadro
+ * dartboard.
+ * <p>
+ * Relevant design patterns: immutable object, static factory method.
+ */
+public class QuadroBoard implements Board {
+    private QuadroBoard() {
+    }
+
+
+    /**
+     * Returns a new {@code QuadroBoard}.
+     *
+     * @return a new {@code QuadroBoard}
+     */
+    public static Board create() {
+        return new QuadroBoard();
+    }
+
+    @Override
+    public List<Field> getFields(FieldType fieldType) {
+        return null;
+    }
+}
