2ab71f856f136199ad1774d3076311fa689c44ca
Maurits Silvis <mauritssilvis@gmail.com>
2023-02-05 20:37:36 +0100

    Add the ability to compare map-based checkout tables

--- java/src/main/java/nl/mauritssilvis/darts/java/tables/map/MapBasedCheckoutTable.java
+++ java/src/main/java/nl/mauritssilvis/darts/java/tables/map/MapBasedCheckoutTable.java
@@ -5,6 +5,7 @@
 
 package nl.mauritssilvis.darts.java.tables.map;
 
+import lombok.EqualsAndHashCode;
 import nl.mauritssilvis.darts.java.checkouts.Checkout;
 import nl.mauritssilvis.darts.java.settings.BoardType;
 import nl.mauritssilvis.darts.java.settings.CheckType;
@@ -22,6 +23,7 @@ import java.util.stream.Collectors;
  * <p>
  * Relevant design patterns: immutable object, static factory method.
  */
+@EqualsAndHashCode
 public final class MapBasedCheckoutTable implements CheckoutTable {
     private final BoardType boardType;
     private final CheckType checkInType;
