ac46e1e18ac6cb63ef6c65e36bf4688ff40cda39
Maurits Silvis <mauritssilvis@gmail.com>
2023-02-06 16:17:49 +0100

    Summarize the help command tests

--- java/src/test/java/nl/mauritssilvis/darts/java/cli/DartsTests.java
+++ java/src/test/java/nl/mauritssilvis/darts/java/cli/DartsTests.java
@@ -7,46 +7,19 @@ package nl.mauritssilvis.darts.java.cli;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import picocli.CommandLine;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
 class DartsTests {
-    @Test
-    void getHelp() {
+    @ParameterizedTest
+    @ValueSource(strings = {"-h", "help"})
+    void getHelp(String arg) {
         Darts darts = new Darts();
-        String[] args = {"-h"};
-
-        StringWriter stringWriter = new StringWriter();
-
-        CommandLine commandLine = new CommandLine(darts);
-        commandLine.setOut(new PrintWriter(stringWriter));
-        commandLine.execute(args);
-
-        String actual = stringWriter.toString();
-
-        String expected1 = "Usage";
-        String expected2 = "darts";
-        String expected3 = "computational toolbox";
-        String expected4 = "Copyright";
-        String expected5 = "2023";
-        String expected6 = "GPL";
-
-        Assertions.assertAll(
-                () -> Assertions.assertTrue(actual.contains(expected1)),
-                () -> Assertions.assertTrue(actual.contains(expected2)),
-                () -> Assertions.assertTrue(actual.contains(expected3)),
-                () -> Assertions.assertTrue(actual.contains(expected4)),
-                () -> Assertions.assertTrue(actual.contains(expected5)),
-                () -> Assertions.assertTrue(actual.contains(expected6))
-        );
-    }
-
-    @Test
-    void getSubCommandHelp() {
-        Darts darts = new Darts();
-        String[] args = {"help"};
+        String[] args = {arg};
 
         StringWriter stringWriter = new StringWriter();
 
