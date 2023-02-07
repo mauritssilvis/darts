ad9cfeefc904fa2703e90047da24e759642ecd7a
Maurits Silvis <mauritssilvis@gmail.com>
2023-02-07 23:22:27 +0100

    Remove empty span elements from the HTML table serialization test

--- java/src/test/java/nl/mauritssilvis/darts/java/tables/output/HtmlTableSerializerTests.java
+++ java/src/test/java/nl/mauritssilvis/darts/java/tables/output/HtmlTableSerializerTests.java
@@ -45,18 +45,18 @@ class HtmlTableSerializerTests {
                         <table>
                           <tr><th class="s">Score</th><th class="t">                                                   1</th><th class="t">                                                   2</th><th class="m"> #</th></tr>
                           <tr><td class="s">   22</td><td class="t">                                                   *</td><td class="t">                                                   *</td><td class="m"> 1</td></tr>
-                          <tr><td class="s">     </td><td class="t"><span class="f">   </span><span class="f">D11</span></td><td class="t">                                                   -</td><td class="m"> 1</td></tr>
+                          <tr><td class="s">     </td><td class="t">                          <span class="f">D11</span></td><td class="t">                                                   -</td><td class="m"> 1</td></tr>
                           <tr><td class="s">   23</td><td class="t">                                                   *</td><td class="t">                                                   *</td><td class="m">38</td></tr>
-                          <tr><td class="s">     </td><td class="t"><span class="f">   </span><span class="f">D11</span></td><td class="t"><span class="f">   </span><span class="f">  1</span></td><td class="m"> 2</td></tr>
-                          <tr><td class="s">     </td><td class="t"><span class="f"> 20</span><span class="f">D10</span></td><td class="t"><span class="f">   </span><span class="f">  3</span></td><td class="m"> 4</td></tr>
-                          <tr><td class="s">     </td><td class="t"><span class="f">   </span><span class="f"> 19</span></td><td class="t"><span class="f">  4</span><span class="f"> D2</span></td><td class="m"> 4</td></tr>
-                          <tr><td class="s">     </td><td class="t"><span class="f"> 18</span><span class="f"> D9</span></td><td class="t"><span class="f">   </span><span class="f">  5</span></td><td class="m"> 4</td></tr>
-                          <tr><td class="s">     </td><td class="t"><span class="f">   </span><span class="f"> 17</span></td><td class="t"><span class="f">  6</span><span class="f"> D3</span></td><td class="m"> 4</td></tr>
-                          <tr><td class="s">     </td><td class="t"><span class="f"> 16</span><span class="f"> D8</span></td><td class="t"><span class="f">   </span><span class="f">  7</span></td><td class="m"> 4</td></tr>
-                          <tr><td class="s">     </td><td class="t"><span class="f">   </span><span class="f"> 15</span></td><td class="t"><span class="f">  8</span><span class="f"> D4</span></td><td class="m"> 4</td></tr>
-                          <tr><td class="s">     </td><td class="t"><span class="f"> 14</span><span class="f"> D7</span></td><td class="t"><span class="f">   </span><span class="f">  9</span></td><td class="m"> 4</td></tr>
-                          <tr><td class="s">     </td><td class="t"><span class="f">   </span><span class="f"> 13</span></td><td class="t"><span class="f"> 10</span><span class="f"> D5</span></td><td class="m"> 4</td></tr>
-                          <tr><td class="s">     </td><td class="t"><span class="f"> 12</span><span class="f"> D6</span></td><td class="t"><span class="f">   </span><span class="f"> 11</span></td><td class="m"> 4</td></tr>
+                          <tr><td class="s">     </td><td class="t">                          <span class="f">D11</span></td><td class="t">                          <span class="f">  1</span></td><td class="m"> 2</td></tr>
+                          <tr><td class="s">     </td><td class="t"><span class="f"> 20</span><span class="f">D10</span></td><td class="t">                          <span class="f">  3</span></td><td class="m"> 4</td></tr>
+                          <tr><td class="s">     </td><td class="t">                          <span class="f"> 19</span></td><td class="t"><span class="f">  4</span><span class="f"> D2</span></td><td class="m"> 4</td></tr>
+                          <tr><td class="s">     </td><td class="t"><span class="f"> 18</span><span class="f"> D9</span></td><td class="t">                          <span class="f">  5</span></td><td class="m"> 4</td></tr>
+                          <tr><td class="s">     </td><td class="t">                          <span class="f"> 17</span></td><td class="t"><span class="f">  6</span><span class="f"> D3</span></td><td class="m"> 4</td></tr>
+                          <tr><td class="s">     </td><td class="t"><span class="f"> 16</span><span class="f"> D8</span></td><td class="t">                          <span class="f">  7</span></td><td class="m"> 4</td></tr>
+                          <tr><td class="s">     </td><td class="t">                          <span class="f"> 15</span></td><td class="t"><span class="f">  8</span><span class="f"> D4</span></td><td class="m"> 4</td></tr>
+                          <tr><td class="s">     </td><td class="t"><span class="f"> 14</span><span class="f"> D7</span></td><td class="t">                          <span class="f">  9</span></td><td class="m"> 4</td></tr>
+                          <tr><td class="s">     </td><td class="t">                          <span class="f"> 13</span></td><td class="t"><span class="f"> 10</span><span class="f"> D5</span></td><td class="m"> 4</td></tr>
+                          <tr><td class="s">     </td><td class="t"><span class="f"> 12</span><span class="f"> D6</span></td><td class="t">                          <span class="f"> 11</span></td><td class="m"> 4</td></tr>
                         </table>
                         """,
                 output);
