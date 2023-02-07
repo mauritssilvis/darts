5ba288cc7ffdbcd433ef159e1155eff7aa99c073
Maurits Silvis <mauritssilvis@gmail.com>
2023-02-07 14:08:09 +0100

    Extend the Markdown serialization tests

--- java/src/test/java/nl/mauritssilvis/darts/java/boards/output/MarkdownBoardSerializerTests.java
+++ java/src/test/java/nl/mauritssilvis/darts/java/boards/output/MarkdownBoardSerializerTests.java
@@ -17,7 +17,7 @@ import org.junit.jupiter.params.provider.EnumSource;
 class MarkdownBoardSerializerTests {
     @Test
     void getAMarkdownBoard() {
-        BoardType boardType = BoardType.QUADRO;
+        BoardType boardType = BoardType.LONDON;
         Board board = BoardFactory.create(boardType);
         Serializer<Board> serializer = MarkdownBoardSerializer.create();
 
@@ -25,36 +25,36 @@ class MarkdownBoardSerializerTests {
 
         Assertions.assertEquals(
                 """
-                        |   S |   D |   T |   Q |
-                        |----:|----:|----:|----:|
-                        |   1 |  D1 |  T1 |  Q1 |
-                        |   2 |  D2 |  T2 |  Q2 |
-                        |   3 |  D3 |  T3 |  Q3 |
-                        |   4 |  D4 |  T4 |  Q4 |
-                        |   5 |  D5 |  T5 |  Q5 |
-                        |   6 |  D6 |  T6 |  Q6 |
-                        |   7 |  D7 |  T7 |  Q7 |
-                        |   8 |  D8 |  T8 |  Q8 |
-                        |   9 |  D9 |  T9 |  Q9 |
-                        |  10 | D10 | T10 | Q10 |
-                        |  11 | D11 | T11 | Q11 |
-                        |  12 | D12 | T12 | Q12 |
-                        |  13 | D13 | T13 | Q13 |
-                        |  14 | D14 | T14 | Q14 |
-                        |  15 | D15 | T15 | Q15 |
-                        |  16 | D16 | T16 | Q16 |
-                        |  17 | D17 | T17 | Q17 |
-                        |  18 | D18 | T18 | Q18 |
-                        |  19 | D19 | T19 | Q19 |
-                        |  20 | D20 | T20 | Q20 |
-                        |  25 | D25 |   - |   - |
+                        |   S |   D |   T |
+                        |----:|----:|----:|
+                        |   1 |  D1 |  T1 |
+                        |   2 |  D2 |  T2 |
+                        |   3 |  D3 |  T3 |
+                        |   4 |  D4 |  T4 |
+                        |   5 |  D5 |  T5 |
+                        |   6 |  D6 |  T6 |
+                        |   7 |  D7 |  T7 |
+                        |   8 |  D8 |  T8 |
+                        |   9 |  D9 |  T9 |
+                        |  10 | D10 | T10 |
+                        |  11 | D11 | T11 |
+                        |  12 | D12 | T12 |
+                        |  13 | D13 | T13 |
+                        |  14 | D14 | T14 |
+                        |  15 | D15 | T15 |
+                        |  16 | D16 | T16 |
+                        |  17 | D17 | T17 |
+                        |  18 | D18 | T18 |
+                        |  19 | D19 | T19 |
+                        |  20 | D20 | T20 |
+                        |  25 | D25 |   - |
                         """,
                 output);
     }
 
     @ParameterizedTest
     @EnumSource(BoardType.class)
-    void getTheTableStart(BoardType boardType) {
+    void getTheBoardStart(BoardType boardType) {
         Board board = BoardFactory.create(boardType);
         Serializer<Board> serializer = MarkdownBoardSerializer.create();
 
--- java/src/test/java/nl/mauritssilvis/darts/java/tables/output/MarkdownTableSerializerTests.java
+++ java/src/test/java/nl/mauritssilvis/darts/java/tables/output/MarkdownTableSerializerTests.java
@@ -14,13 +14,14 @@ import nl.mauritssilvis.darts.java.tables.Table;
 import nl.mauritssilvis.darts.java.tables.TableGenerator;
 import nl.mauritssilvis.darts.java.tables.factory.TableGeneratorFactory;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 
 class MarkdownTableSerializerTests {
-    @ParameterizedTest
-    @EnumSource(TableType.class)
-    void getTheTableStart(TableType tableType) {
+    @Test
+    void getAMarkdownTable() {
+        TableType tableType = TableType.ASCENDING;
         BoardType boardType = BoardType.YORKSHIRE;
         CheckType checkInType = CheckType.ANY;
         CheckType checkoutType = CheckType.ANY;
@@ -39,6 +40,48 @@ class MarkdownTableSerializerTests {
 
         String output = serializer.serialize(table);
 
-        Assertions.assertTrue(output.startsWith("| "));
+        Assertions.assertEquals(
+                """
+                        | Score |       1 |       2 |  # |
+                        |------:|--------:|--------:|---:|
+                        |    22 |       * |       * |  1 |
+                        |       |     D11 |       - |  1 |
+                        |    23 |       * |       * | 38 |
+                        |       |     D11 |       1 |  2 |
+                        |       |  20/D10 |       3 |  4 |
+                        |       |      19 |   4/ D2 |  4 |
+                        |       |  18/ D9 |       5 |  4 |
+                        |       |      17 |   6/ D3 |  4 |
+                        |       |  16/ D8 |       7 |  4 |
+                        |       |      15 |   8/ D4 |  4 |
+                        |       |  14/ D7 |       9 |  4 |
+                        |       |      13 |  10/ D5 |  4 |
+                        |       |  12/ D6 |      11 |  4 |
+                        """,
+                output);
+    }
+
+    @ParameterizedTest
+    @EnumSource(TableType.class)
+    void getTheTableStart(TableType tableType) {
+        BoardType boardType = BoardType.LONDON;
+        CheckType checkInType = CheckType.ANY;
+        CheckType checkoutType = CheckType.DOUBLE;
+        FinderType finderType = FinderType.DESCENDING;
+
+        TableGenerator tableGenerator = TableGeneratorFactory.create(
+                tableType, boardType, checkInType, checkoutType, finderType
+        );
+
+        int minScore = 2;
+        int maxScore = 2;
+
+        Table table = tableGenerator.generate(minScore, maxScore);
+
+        Serializer<Table> serializer = MarkdownTableSerializer.create();
+
+        String output = serializer.serialize(table);
+
+        Assertions.assertTrue(output.startsWith("| Score | "));
     }
 }
