1bf9f807f9bd8e44dc25c5f459de5dc799dce964
Maurits Silvis <mauritssilvis@gmail.com>
2023-02-07 17:50:55 +0100

    Implement HTML board serialization using the board writer template

--- java/src/main/java/nl/mauritssilvis/darts/java/boards/output/HtmlBoardSerializer.java
+++ java/src/main/java/nl/mauritssilvis/darts/java/boards/output/HtmlBoardSerializer.java
@@ -6,7 +6,14 @@
 package nl.mauritssilvis.darts.java.boards.output;
 
 import nl.mauritssilvis.darts.java.boards.Board;
+import nl.mauritssilvis.darts.java.boards.FieldType;
 import nl.mauritssilvis.darts.java.output.Serializer;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * An implementation of the generic {@code Serializer} interface that serializes
@@ -29,8 +36,128 @@ public final class HtmlBoardSerializer implements Serializer<Board> {
 
     @Override
     public String serialize(Board object) {
-        throw new UnsupportedOperationException(
-                "Dartboards do not yet support serialization to HTML."
-        );
+        return new HtmlBoardPrinter(object).print();
+    }
+
+    private static class HtmlBoardPrinter extends BoardPrinter {
+        private final Map<Integer, StringBuilder> builderMap;
+        private final String columnFormat;
+        private int row;
+
+        HtmlBoardPrinter(Board board) {
+            super(board);
+
+            builderMap = new HashMap<>();
+            IntStream.range(0, getNumFields() + 1)
+                    .forEach(i -> builderMap.put(i, new StringBuilder()));
+
+            columnFormat = "%1$" + getFieldWidth() + "s";
+        }
+
+        @Override
+        void startTable() {
+            builderMap.values().forEach(e -> e.append("  <tr>"));
+        }
+
+        @Override
+        void endTable() {
+            builderMap.values().forEach(e -> e.append("</tr>\n"));
+        }
+
+        @Override
+        void startFields(FieldType fieldType) {
+            String typeInitial = getTypeInitial(fieldType);
+
+            builderMap.get(row)
+                    .append("<th>")
+                    .append(String.format(columnFormat, typeInitial))
+                    .append("</th>");
+
+            nextRow();
+        }
+
+        @Override
+        void endFields() {
+        }
+
+        @Override
+        void separateFields() {
+        }
+
+        @Override
+        void endLastFields() {
+        }
+
+        @Override
+        void separateEmptyFields() {
+        }
+
+        @Override
+        void startField() {
+            builderMap.get(row).append("<td>");
+        }
+
+        @Override
+        void addField(String name) {
+            builderMap.get(row).append(String.format(columnFormat, name));
+        }
+
+        @Override
+        void endField() {
+            builderMap.get(row).append("</td>");
+            nextRow();
+        }
+
+        @Override
+        void separateField() {
+        }
+
+        @Override
+        void endLastField() {
+        }
+
+        @Override
+        void startEmptyField() {
+            startField();
+        }
+
+        @Override
+        void addEmptyField() {
+            addField("-");
+        }
+
+        @Override
+        void endEmptyField() {
+            endField();
+        }
+
+        @Override
+        void separateEmptyField() {
+        }
+
+        @Override
+        void endLastEmptyField() {
+        }
+
+        @Override
+        String getString() {
+            return "<table>\n"
+                    + builderMap.entrySet().stream()
+                    .sorted(Comparator.comparingInt(Map.Entry::getKey))
+                    .map(Map.Entry::getValue)
+                    .map(StringBuilder::toString)
+                    .collect(Collectors.joining())
+                    + "</table>\n";
+        }
+
+        private void nextRow() {
+            row = (row + 1) % builderMap.size();
+        }
+
+        private static String getTypeInitial(FieldType fieldType) {
+            String fullName = fieldType.toString();
+            String shortName = fullName.split("\\.")[1];
+            return shortName.substring(0, 1);
+        }
     }
 }
--- java/src/test/java/nl/mauritssilvis/darts/java/boards/output/HtmlBoardSerializerTests.java
+++ java/src/test/java/nl/mauritssilvis/darts/java/boards/output/HtmlBoardSerializerTests.java
@@ -25,30 +25,30 @@ class HtmlBoardSerializerTests {
 
         Assertions.assertEquals(
                 """
-                <table>
-                  <tr><th>  S</th><th>  D</th></tr>
-                  <tr><td>  1</td><td> D1</td></tr>
-                  <tr><td>  2</td><td> D2</td></tr>
-                  <tr><td>  3</td><td> D3</td></tr>
-                  <tr><td>  4</td><td> D4</td></tr>
-                  <tr><td>  5</td><td> D5</td></tr>
-                  <tr><td>  6</td><td> D6</td></tr>
-                  <tr><td>  7</td><td> D7</td></tr>
-                  <tr><td>  8</td><td> D8</td></tr>
-                  <tr><td>  9</td><td> D9</td></tr>
-                  <tr><td> 10</td><td>D10</td></tr>
-                  <tr><td> 11</td><td>D11</td></tr>
-                  <tr><td> 12</td><td>D12</td></tr>
-                  <tr><td> 13</td><td>D13</td></tr>
-                  <tr><td> 14</td><td>D14</td></tr>
-                  <tr><td> 15</td><td>D15</td></tr>
-                  <tr><td> 16</td><td>D16</td></tr>
-                  <tr><td> 17</td><td>D17</td></tr>
-                  <tr><td> 18</td><td>D18</td></tr>
-                  <tr><td> 19</td><td>D19</td></tr>
-                  <tr><td> 20</td><td>D20</td></tr>
-                  <tr><td>  -</td><td>D25</td></tr>
-                </table>
+                        <table>
+                          <tr><th>  S</th><th>  D</th></tr>
+                          <tr><td>  1</td><td> D1</td></tr>
+                          <tr><td>  2</td><td> D2</td></tr>
+                          <tr><td>  3</td><td> D3</td></tr>
+                          <tr><td>  4</td><td> D4</td></tr>
+                          <tr><td>  5</td><td> D5</td></tr>
+                          <tr><td>  6</td><td> D6</td></tr>
+                          <tr><td>  7</td><td> D7</td></tr>
+                          <tr><td>  8</td><td> D8</td></tr>
+                          <tr><td>  9</td><td> D9</td></tr>
+                          <tr><td> 10</td><td>D10</td></tr>
+                          <tr><td> 11</td><td>D11</td></tr>
+                          <tr><td> 12</td><td>D12</td></tr>
+                          <tr><td> 13</td><td>D13</td></tr>
+                          <tr><td> 14</td><td>D14</td></tr>
+                          <tr><td> 15</td><td>D15</td></tr>
+                          <tr><td> 16</td><td>D16</td></tr>
+                          <tr><td> 17</td><td>D17</td></tr>
+                          <tr><td> 18</td><td>D18</td></tr>
+                          <tr><td> 19</td><td>D19</td></tr>
+                          <tr><td> 20</td><td>D20</td></tr>
+                          <tr><td>  -</td><td>D25</td></tr>
+                        </table>
                         """,
                 output);
     }
