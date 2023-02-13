07082d5e99ef5b6eefa883f76ad35148432e842a
Maurits Silvis <mauritssilvis@gmail.com>
2023-02-13 03:26:39 +0100

    Implement string serializers using the pretty formatter

--- java/src/main/java/nl/mauritssilvis/darts/java/boards/output/StringBoardSerializer.java
+++ java/src/main/java/nl/mauritssilvis/darts/java/boards/output/StringBoardSerializer.java
@@ -6,17 +6,26 @@
 package nl.mauritssilvis.darts.java.boards.output;
 
 import nl.mauritssilvis.darts.java.boards.Board;
+import nl.mauritssilvis.darts.java.output.Formatter;
 import nl.mauritssilvis.darts.java.output.Serializer;
-import nl.mauritssilvis.darts.java.output.StringSerializer;
+import nl.mauritssilvis.darts.java.output.pretty.PrettyFormatter;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 /**
- * An implementation of the generic {@code StringSerializer} interface that
- * allows for the creation of objects that can serialize {@code Board} objects
- * using their Java string representation.
+ * An implementation of the generic {@code Serializer} interface that serializes
+ * {@code Board} objects using their Java string representation.
  * <p>
  * Relevant design patterns: strategy, immutable object, static factory method.
  */
-public final class StringBoardSerializer implements StringSerializer<Board> {
+public final class StringBoardSerializer implements Serializer<Board> {
+    private static final int indentationSize = 2;
+    private final Collection<Character> brackets = List.of('{', '[', '(');
+    private final Collection<Character> delimiters = Collections.singleton(',');
+    private final Formatter formatter = PrettyFormatter.of(brackets, delimiters, indentationSize);
+
     private StringBoardSerializer() {
     }
 
@@ -27,5 +36,10 @@ public final class StringBoardSerializer implements StringSerializer<Board> {
      */
     public static Serializer<Board> create() {
         return new StringBoardSerializer();
+    }
+
+    @Override
+    public String serialize(Board object) {
+        return formatter.format(object.toString());
     }
 }
--- java/src/main/java/nl/mauritssilvis/darts/java/tables/output/StringTableSerializer.java
+++ java/src/main/java/nl/mauritssilvis/darts/java/tables/output/StringTableSerializer.java
@@ -5,18 +5,27 @@
 
 package nl.mauritssilvis.darts.java.tables.output;
 
+import nl.mauritssilvis.darts.java.output.Formatter;
 import nl.mauritssilvis.darts.java.output.Serializer;
-import nl.mauritssilvis.darts.java.output.StringSerializer;
+import nl.mauritssilvis.darts.java.output.pretty.PrettyFormatter;
 import nl.mauritssilvis.darts.java.tables.Table;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 /**
- * An implementation of the generic {@code StringSerializer} interface that
- * allows for the creation of objects that can serialize {@code Table} objects
- * using their Java string representation.
+ * An implementation of the generic {@code Serializer} interface that serializes
+ * {@code Table} objects using their Java string representation.
  * <p>
  * Relevant design patterns: strategy, immutable object, static factory method.
  */
-public final class StringTableSerializer implements StringSerializer<Table> {
+public final class StringTableSerializer implements Serializer<Table> {
+    private static final int indentationSize = 2;
+    private final Collection<Character> brackets = List.of('{', '[', '(');
+    private final Collection<Character> delimiters = Collections.singleton(',');
+    private final Formatter formatter = PrettyFormatter.of(brackets, delimiters, indentationSize);
+
     private StringTableSerializer() {
     }
 
@@ -27,5 +36,10 @@ public final class StringTableSerializer implements StringSerializer<Table> {
      */
     public static Serializer<Table> create() {
         return new StringTableSerializer();
+    }
+
+    @Override
+    public String serialize(Table object) {
+        return formatter.format(object.toString());
     }
 }
