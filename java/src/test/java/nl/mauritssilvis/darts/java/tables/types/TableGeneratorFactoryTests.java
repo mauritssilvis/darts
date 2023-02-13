a3dbe63b4dc2dbba91201b74c2c9c86c2b144171
Maurits Silvis <mauritssilvis@gmail.com>
2023-02-13 21:12:43 +0100

    Check passing of arguments to the table generator factory

--- java/src/test/java/nl/mauritssilvis/darts/java/tables/types/TableGeneratorFactoryTests.java
+++ java/src/test/java/nl/mauritssilvis/darts/java/tables/types/TableGeneratorFactoryTests.java
@@ -8,6 +8,7 @@ package nl.mauritssilvis.darts.java.tables.types;
 import nl.mauritssilvis.darts.java.settings.Settings;
 import nl.mauritssilvis.darts.java.settings.TableType;
 import nl.mauritssilvis.darts.java.settings.types.TableSettingsBuilder;
+import nl.mauritssilvis.darts.java.tables.Table;
 import nl.mauritssilvis.darts.java.tables.TableGenerator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -24,5 +25,23 @@ class TableGeneratorFactoryTests {
         TableGenerator tableGenerator = TableGeneratorFactory.create(tableType, settings);
 
         Assertions.assertTrue(tableGenerator instanceof AscendingTableGenerator);
+    }
+
+    @Test
+    void passOnTheSettings() {
+        TableType tableType = TableType.ASCENDING;
+
+        Settings settings = TableSettingsBuilder.create()
+                .setTableType(tableType)
+                .build();
+
+        TableGenerator tableGenerator = TableGeneratorFactory.create(tableType, settings);
+
+        int minScore = 0;
+        int maxScore = 0;
+
+        Table table = tableGenerator.generate(minScore, maxScore);
+
+        Assertions.assertEquals(settings, table.getSettings());
     }
 }
