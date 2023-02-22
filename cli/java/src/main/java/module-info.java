8c486d48a4ee0e4a9f4d517fb087bac6a8dd013f
Maurits Silvis <mauritssilvis@gmail.com>
2023-02-22 22:59:47 +0100

    Add a module descriptor

---
+++ cli/java/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * A Java-based command-line interface for {@code darts}, a computational
+ * toolbox aimed at the game of darts.
+ *
+ * @author Maurits Silvis
+ * @since 0.1.0
+ */
+module nl.mauritssilvis.darts.java.cli {
+    requires lombok;
+    requires info.picocli;
+}
