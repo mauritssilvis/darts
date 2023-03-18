deaa96e1408bf553883d89c4a253b32b473cede6
Maurits Silvis <mauritssilvis@gmail.com>
2023-03-18 13:58:43 +0100

    Store the project version in the manifest file

--- cli/java/build.gradle.kts
+++ cli/java/build.gradle.kts
@@ -114,7 +114,11 @@ tasks {
 
     jar {
         manifest {
-            attributes("Main-Class" to "nl.mauritssilvis.darts.java.cli.DartsApp")
+            attributes(
+                    "Implementation-Title" to "${project.name}",
+                    "Implementation-Version" to "${project.version}",
+                    "Main-Class" to "nl.mauritssilvis.darts.java.cli.DartsApp"
+            )
         }
     }
 
