1ecbd59c658e43dad1ca3a274b7c27059b7bbbf6
Maurits Silvis <mauritssilvis@gmail.com>
2023-03-18 17:27:08 +0100

    Set up a separate darts API project

---
+++ api/java-darts-api/build.gradle.kts
@@ -0,0 +1,11 @@
+/*
+ * Copyright © 2023 Maurits Silvis
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+plugins {
+    `java-library`
+}
+
+group = "nl.mauritssilvis.darts.java"
+version = "0.5.0-SNAPSHOT"
