2caebcf34e9f472e371cbc32239f9556b5740b2b
Maurits Silvis <mauritssilvis@gmail.com>
2023-03-18 15:27:58 +0100

    Set latest versions

--- api/java-darts-api/build.gradle.kts
+++ api/java-darts-api/build.gradle.kts
@@ -3,15 +3,15 @@ plugins {
 }
 
 group = "nl.mauritssilvis.darts.java"
-version = "1.0-SNAPSHOT"
+version = "0.4.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
 }
 
 tasks {
