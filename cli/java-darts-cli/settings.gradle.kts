c5b17ce15c922664ae1ce825cf0604cca29de582
Maurits Silvis <mauritssilvis@gmail.com>
2023-03-19 16:11:14 +0100

    Include toolchain resolvers in all projects

--- cli/java-darts-cli/settings.gradle.kts
+++ cli/java-darts-cli/settings.gradle.kts
@@ -3,11 +3,11 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-rootProject.name = "java-darts-cli"
-
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
 }
+
+rootProject.name = "java-darts-cli"
 
 val apiDir = "../../api/java-darts-api"
 
--- core/java-darts-core/settings.gradle.kts
+++ core/java-darts-core/settings.gradle.kts
@@ -3,6 +3,10 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
+}
+
 rootProject.name = "java-darts-core"
 
 val apiDir = "../../api/java-darts-api"
