ffc49c00df47385ab5b8f2175fb8f98767856257
Maurits Silvis <mauritssilvis@gmail.com>
2023-08-22 21:42:15 +0200

    Update the refreshVersions version

--- core/java-darts-core/settings.gradle.kts
+++ core/java-darts-core/settings.gradle.kts
@@ -5,7 +5,7 @@
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
-    id("de.fayard.refreshVersions") version "0.51.0"
+    id("de.fayard.refreshVersions") version "0.60.0"
 }
 
 rootProject.name = "java-darts-core"
