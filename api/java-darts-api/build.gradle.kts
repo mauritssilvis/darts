3d2bf96671eec6c7aa32dbc1b2bd697224d1745b
Maurits Silvis <mauritssilvis@gmail.com>
2023-10-11 19:33:09 +0200

    Select the Temurin JDK 21

--- api/java-darts-api/build.gradle.kts
+++ api/java-darts-api/build.gradle.kts
@@ -26,8 +26,7 @@ dependencies {
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(libs.versions.java.jdk.get()))
-        // TODO: Reset to JvmVendorSpec.ADOPTIUM
-        vendor.set(JvmVendorSpec.ORACLE)
+        vendor.set(JvmVendorSpec.ADOPTIUM)
     }
 
     withSourcesJar()
